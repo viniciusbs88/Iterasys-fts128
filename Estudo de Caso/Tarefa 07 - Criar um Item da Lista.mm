5ba52304c132a08ee5d3f3831cce1e34b91dd74e
viniciusbs88 <66048403+viniciusbs88@users.noreply.github.com>
2020-10-26 20:33:22 -0300

    Commit inicial
    Criada a pasta Estudo de Caso e incluídos os arquivos referentes às tarefas realizadas até o dia 26/10/2020.

---
+++ Estudo de Caso/Tarefa 07 - Criar um Item da Lista.mm
@@ -0,0 +1,43 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1603674511575" ID="ID_318903422" MODIFIED="1603674529186" TEXT="Criar um Item da Lista">
+<node CREATED="1603674547259" ID="ID_409863737" MODIFIED="1603674588288" POSITION="right" TEXT="Consulte uma lista">
+<node CREATED="1603674659009" ID="ID_568602634" MODIFIED="1603674676843" TEXT="Se a lista j&#xe1; existe, selecione-a"/>
+<node CREATED="1603674677686" ID="ID_929570577" MODIFIED="1603674688167" TEXT="Se a lista n&#xe3;o existe, crie-a">
+<node CREATED="1603674688169" ID="ID_764393406" MODIFIED="1603674692053" TEXT="Preencha nome"/>
+<node CREATED="1603674692820" ID="ID_28384456" MODIFIED="1603674700979" TEXT="Salve"/>
+</node>
+</node>
+<node CREATED="1603674588869" ID="ID_232217839" MODIFIED="1603674614447" POSITION="right" TEXT="Adicione um produto">
+<node CREATED="1603674614448" ID="ID_849000360" MODIFIED="1603674624196" TEXT="Pesquise o produto pelo nome">
+<node CREATED="1603674716007" ID="ID_449486593" MODIFIED="1603674724925" TEXT="Se o produto existe, selecione-o"/>
+<node CREATED="1603674725938" ID="ID_602862999" MODIFIED="1603674740353" TEXT="Se o produto n&#xe3;o existe, cadastre-o">
+<node CREATED="1603674740354" ID="ID_1074717149" MODIFIED="1603674745281" TEXT="Preencha nome"/>
+<node CREATED="1603674746054" ID="ID_123866078" MODIFIED="1603674751627" TEXT="Preencha marca"/>
+<node CREATED="1603674752030" ID="ID_424271147" MODIFIED="1603674764551" TEXT="Preencha tamanho / medida"/>
+<node CREATED="1603674765940" ID="ID_966232884" MODIFIED="1603674784768" TEXT="Preencha categoria"/>
+<node CREATED="1603674865622" ID="ID_132141462" MODIFIED="1603674874999" TEXT="Preencha descri&#xe7;&#xe3;o"/>
+</node>
+</node>
+<node CREATED="1603674625086" ID="ID_1444693675" MODIFIED="1603674647293" TEXT="Defina a quantidade a ser comprada"/>
+<node CREATED="1603674894629" ID="ID_900436441" MODIFIED="1603674910301" TEXT="Atualize o valor do pre&#xe7;o"/>
+<node CREATED="1603674961365" ID="ID_447480875" MODIFIED="1603674981215" TEXT="Defina o local da compra"/>
+<node CREATED="1603674988149" ID="ID_958612854" MODIFIED="1603675002090" TEXT="Defina se foi ou n&#xe3;o comprado">
+<node CREATED="1603675009360" ID="ID_412283140" MODIFIED="1603675076852" TEXT="Poderia ser um booleano">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1603675031477" ID="ID_567256832" MODIFIED="1603675080798" TEXT="Poderia ser derivado do local da compra">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1603675043878" ID="ID_72574547" MODIFIED="1603675050000" TEXT="se h&#xe1; local, foi comprado"/>
+<node CREATED="1603675050505" ID="ID_697758775" MODIFIED="1603675061731" TEXT="se n&#xe3;o h&#xe1; local, ainda precisa ser comprado"/>
+</node>
+</node>
+<node CREATED="1603674926557" ID="ID_1868812928" MODIFIED="1603674937820" TEXT="Salve o produto na lista"/>
+<node CREATED="1603675097266" ID="ID_1267248008" MODIFIED="1603675108845" TEXT="Verifique se a lista est&#xe1; atualizada">
+<node CREATED="1603675108848" ID="ID_1326694683" MODIFIED="1603675131733" TEXT="Possui um item a mais que a quantidade anterior &#xe0; opera&#xe7;&#xe3;o"/>
+<node CREATED="1603675132568" ID="ID_1105945475" MODIFIED="1603675145955" TEXT="Possui o item rec&#xe9;m cadastrado"/>
+<node CREATED="1603675146303" ID="ID_648506718" MODIFIED="1603675188017" TEXT="Valor total da lista de itens est&#xe1; atualizado com o valor do produto acrescido ao valor total anterior &#xe0; opera&#xe7;&#xe3;o"/>
+</node>
+</node>
+</node>
+</map>
