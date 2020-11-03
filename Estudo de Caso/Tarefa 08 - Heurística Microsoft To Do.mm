afbd0c734cc614495511edf45171634742e3132a
viniciusbs88 <66048403+viniciusbs88@users.noreply.github.com>
2020-11-02 22:29:32 -0300

    Resposta da Tarefa 08
    Mapas mentais e html criado através da extensão Test & Feedback

---
+++ Estudo de Caso/Tarefa 08 - Heurística Microsoft To Do.mm
@@ -0,0 +1,150 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1604284105192" ID="ID_237447013" MODIFIED="1604284117918" TEXT="Microsoft To Do">
+<node CREATED="1604284170694" ID="ID_1674356003" MODIFIED="1604284173901" POSITION="right" TEXT="Heur&#xed;stica">
+<node CREATED="1604284177232" ID="ID_1247465078" MODIFIED="1604284260478" TEXT="Lista de Itens">
+<node CREATED="1604284260482" ID="ID_1525183491" MODIFIED="1604284283303" TEXT="Criar uma nova lista">
+<node CREATED="1604284283307" ID="ID_1447517597" MODIFIED="1604284429452" TEXT="Atribuir um nome"/>
+<node CREATED="1604284311326" ID="ID_427978644" MODIFIED="1604284455080" TEXT="Adicionar descri&#xe7;&#xe3;o sobre a lista"/>
+<node CREATED="1604284346697" ID="ID_347334288" MODIFIED="1604284350734" TEXT="Data de Cria&#xe7;&#xe3;o"/>
+</node>
+<node CREATED="1604284323564" ID="ID_498182949" MODIFIED="1604284330490" TEXT="Editar Lista">
+<node CREATED="1604284330492" ID="ID_1576913987" MODIFIED="1604284469283" TEXT="Editar o nome de uma lista criada"/>
+<node CREATED="1604284334220" ID="ID_746558944" MODIFIED="1604284487363" TEXT="Editar a descri&#xe7;&#xe3;o sobre a lista"/>
+</node>
+<node CREATED="1604284835743" ID="ID_1593643548" MODIFIED="1604284850814" TEXT="Totalizar valor da compra baseado nos itens marcados como comprados"/>
+<node CREATED="1604285025112" ID="ID_564101204" MODIFIED="1604285039062" TEXT="Excluir Lista">
+<node CREATED="1604285039062" ID="ID_843120231" MODIFIED="1604285051112" TEXT="Confirmar se usu&#xe1;rio deseja excluir lista"/>
+</node>
+</node>
+<node CREATED="1604284492624" ID="ID_1554561388" MODIFIED="1604284497110" TEXT="Itens">
+<node CREATED="1604284497110" ID="ID_1178742725" MODIFIED="1604284516594" TEXT="Incluir um novo item">
+<node CREATED="1604284516596" ID="ID_768577151" MODIFIED="1604284553168" TEXT="Registrar informa&#xe7;&#xf5;es do item e deix&#xe1;-lo dispon&#xed;vel na lista e da base de dados"/>
+<node CREATED="1603649168207" ID="ID_940615129" MODIFIED="1604284612169" TEXT="Campos">
+<node CREATED="1603649200016" ID="ID_101898843" MODIFIED="1603649202317" TEXT="Nome"/>
+<node CREATED="1603649202607" ID="ID_672683631" MODIFIED="1603649205152" TEXT="Descri&#xe7;&#xe3;o"/>
+<node CREATED="1603649205385" ID="ID_1125059537" MODIFIED="1603649210492" TEXT="Marca"/>
+<node CREATED="1603649210691" ID="ID_379309321" MODIFIED="1603649214812" TEXT="Categoria"/>
+<node CREATED="1603649215066" ID="ID_578974968" MODIFIED="1603649261542" TEXT="Tamanho / Medida"/>
+</node>
+</node>
+<node CREATED="1604284685778" ID="ID_356877818" MODIFIED="1604284696597" TEXT="Adicionar Item na lista">
+<node CREATED="1604284696598" ID="ID_617235942" MODIFIED="1604284737939" TEXT="Associar um item da base na lista atual"/>
+<node CREATED="1604284743585" ID="ID_608540171" MODIFIED="1604284819067" TEXT="Registrar um pre&#xe7;o e uma quantidade ao item associado"/>
+<node CREATED="1604284821184" ID="ID_1399694119" MODIFIED="1604284864656" TEXT="Marcar se o item foi comprado ou n&#xe3;o"/>
+</node>
+<node CREATED="1604284975154" ID="ID_578508311" MODIFIED="1604284981322" TEXT="Excluir item">
+<node CREATED="1604284983442" ID="ID_1191318451" MODIFIED="1604284996604" TEXT="Confirmar se usu&#xe1;rio deseja excluir item"/>
+</node>
+</node>
+</node>
+<node CREATED="1604285146105" ID="ID_701642581" MODIFIED="1604285162768" POSITION="left" TEXT="Funcionamento">
+<node CREATED="1604285164538" ID="ID_942719292" MODIFIED="1604287449035" TEXT="Web">
+<node CREATED="1604284177232" ID="ID_29384834" MODIFIED="1604284260478" TEXT="Lista de Itens">
+<node CREATED="1604284260482" ID="ID_938891609" MODIFIED="1604284283303" TEXT="Criar uma nova lista">
+<node CREATED="1604284283307" ID="ID_1636676824" MODIFIED="1604285607981" TEXT="Cria&#xe7;&#xe3;o f&#xe1;cil, bastando atribuir nome &#xe0; lista na op&#xe7;&#xe3;o &quot;Nova lista&quot; do menu lateral esquerdo">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1604284311326" ID="ID_864867099" MODIFIED="1604285613714" TEXT="N&#xe3;o h&#xe1; descri&#xe7;&#xe3;o da lista">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1604284346697" ID="ID_161345546" MODIFIED="1604285616728" TEXT="N&#xe3;o h&#xe1; identifica&#xe7;&#xe3;o da data de cria&#xe7;&#xe3;o">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1604284323564" ID="ID_1662733411" MODIFIED="1604284330490" TEXT="Editar Lista">
+<node CREATED="1604284330492" ID="ID_1275890020" MODIFIED="1604285802692" TEXT="A edi&#xe7;&#xe3;o do nome da lista &#xe9; simples, por&#xe9;m &#xe9; realizada com duplo clique no nome apresentado na &#xe1;rea central da tela. De acordo com a forma de cria&#xe7;&#xe3;o, o usu&#xe1;rio pode ser levado a achar que conseguir&#xe1; editar o nome pelo menu lateral">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1604284835743" ID="ID_1489833762" MODIFIED="1604286870330" TEXT="Como cada item da lista &#xe9; considerado uma tarefa no sistema, n&#xe3;o h&#xe1; previs&#xe3;o de campos para descrever as caracter&#xed;sticas de um produto a ser inclu&#xed;do numa lista de compras. Por isso, n&#xe3;o h&#xe1; como totalizar um valor de compra.">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1604285025112" ID="ID_1111720517" MODIFIED="1604285039062" TEXT="Excluir Lista">
+<node CREATED="1604285039062" ID="ID_1282364623" MODIFIED="1604286897313" TEXT="Confirmar se usu&#xe1;rio deseja excluir lista">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1604284492624" ID="ID_393761984" MODIFIED="1604284497110" TEXT="Itens">
+<node CREATED="1604284497110" ID="ID_969018298" MODIFIED="1604284516594" TEXT="Incluir um novo item">
+<node CREATED="1604284516596" ID="ID_32439839" MODIFIED="1604287265336" TEXT="N&#xe3;o h&#xe1; uma base de itens para servir de &quot;acervo&quot; de modo que possa ser reaproveitado. Apesar disso, &#xe9; poss&#xed;vel copiar um item de uma lista para a outra.">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1603649168207" FOLDED="true" ID="ID_501296896" MODIFIED="1604287268498" TEXT="Campos">
+<node CREATED="1603649200016" ID="ID_1561134949" MODIFIED="1604286913032" TEXT="Nome">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1603649202607" ID="ID_416838564" MODIFIED="1604287040018" TEXT="Descri&#xe7;&#xe3;o">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1604287114998" ID="ID_1285826863" MODIFIED="1604287140921" TEXT="Demais informa&#xe7;&#xf5;es precisam ser cadastradas com a descri&#xe7;&#xe3;o no campo &quot;Anota&#xe7;&#xf5;es&quot;"/>
+</node>
+<node CREATED="1603649205385" ID="ID_811683859" MODIFIED="1604287101737" TEXT="Marca">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1603649210691" ID="ID_1495139936" MODIFIED="1604287101737" TEXT="Categoria">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1603649215066" ID="ID_829129871" MODIFIED="1604287101737" TEXT="Tamanho / Medida">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+</node>
+<node CREATED="1604284685778" ID="ID_392038454" MODIFIED="1604284696597" TEXT="Adicionar Item na lista">
+<node CREATED="1604284696598" ID="ID_1201780159" MODIFIED="1604287365988" TEXT="Qualquer informa&#xe7;&#xe3;o sobre um item precisa ser cadastrada nas anota&#xe7;&#xf5;es, n&#xe3;o havendo campos espec&#xed;ficos.">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1604284821184" ID="ID_336110487" MODIFIED="1604287360083" TEXT="O item pode ser marcado como &quot;conclu&#xed;do&quot; para indicar que foi comprado">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1604287370077" ID="ID_568833754" MODIFIED="1604287410276" TEXT="C&#xed;rculo vazio permite a interpreta&#xe7;&#xe3;o de que o campo serve para sele&#xe7;&#xe3;o de v&#xe1;rios itens">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+</node>
+<node CREATED="1604284975154" ID="ID_311369846" MODIFIED="1604284981322" TEXT="Excluir item">
+<node CREATED="1604284983442" ID="ID_1996141521" MODIFIED="1604287430414" TEXT="Confirmar se usu&#xe1;rio deseja excluir item">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1604285169486" ID="ID_1656220091" MODIFIED="1604285172845" TEXT="Mobile">
+<node CREATED="1604284177232" ID="ID_637277998" MODIFIED="1604284260478" TEXT="Lista de Itens">
+<node CREATED="1604284260482" ID="ID_660811639" MODIFIED="1604284283303" TEXT="Criar uma nova lista">
+<node CREATED="1604284283307" ID="ID_160005423" MODIFIED="1604284429452" TEXT="Atribuir um nome"/>
+<node CREATED="1604284311326" ID="ID_652038487" MODIFIED="1604284455080" TEXT="Adicionar descri&#xe7;&#xe3;o sobre a lista"/>
+<node CREATED="1604284346697" ID="ID_543115139" MODIFIED="1604284350734" TEXT="Data de Cria&#xe7;&#xe3;o"/>
+</node>
+<node CREATED="1604284323564" ID="ID_1504976718" MODIFIED="1604284330490" TEXT="Editar Lista">
+<node CREATED="1604284330492" ID="ID_1479680320" MODIFIED="1604284469283" TEXT="Editar o nome de uma lista criada"/>
+<node CREATED="1604284334220" ID="ID_1093634310" MODIFIED="1604284487363" TEXT="Editar a descri&#xe7;&#xe3;o sobre a lista"/>
+</node>
+<node CREATED="1604284835743" ID="ID_810275503" MODIFIED="1604284850814" TEXT="Totalizar valor da compra baseado nos itens marcados como comprados"/>
+<node CREATED="1604285025112" ID="ID_1273817348" MODIFIED="1604285039062" TEXT="Excluir Lista">
+<node CREATED="1604285039062" ID="ID_1039096625" MODIFIED="1604285051112" TEXT="Confirmar se usu&#xe1;rio deseja excluir lista"/>
+</node>
+</node>
+<node CREATED="1604284492624" ID="ID_317000423" MODIFIED="1604284497110" TEXT="Itens">
+<node CREATED="1604284497110" ID="ID_107889562" MODIFIED="1604284516594" TEXT="Incluir um novo item">
+<node CREATED="1604284516596" ID="ID_330659580" MODIFIED="1604284553168" TEXT="Registrar informa&#xe7;&#xf5;es do item e deix&#xe1;-lo dispon&#xed;vel na lista e da base de dados"/>
+<node CREATED="1603649168207" ID="ID_234624123" MODIFIED="1604284612169" TEXT="Campos">
+<node CREATED="1603649200016" ID="ID_1152050929" MODIFIED="1603649202317" TEXT="Nome"/>
+<node CREATED="1603649202607" ID="ID_1811230830" MODIFIED="1603649205152" TEXT="Descri&#xe7;&#xe3;o"/>
+<node CREATED="1603649205385" ID="ID_1532929084" MODIFIED="1603649210492" TEXT="Marca"/>
+<node CREATED="1603649210691" ID="ID_652680325" MODIFIED="1603649214812" TEXT="Categoria"/>
+<node CREATED="1603649215066" ID="ID_1042404619" MODIFIED="1603649261542" TEXT="Tamanho / Medida"/>
+</node>
+</node>
+<node CREATED="1604284685778" ID="ID_1799584770" MODIFIED="1604284696597" TEXT="Adicionar Item na lista">
+<node CREATED="1604284696598" ID="ID_1831477501" MODIFIED="1604284737939" TEXT="Associar um item da base na lista atual"/>
+<node CREATED="1604284743585" ID="ID_865116961" MODIFIED="1604284819067" TEXT="Registrar um pre&#xe7;o e uma quantidade ao item associado"/>
+<node CREATED="1604284821184" ID="ID_812399378" MODIFIED="1604284864656" TEXT="Marcar se o item foi comprado ou n&#xe3;o"/>
+</node>
+<node CREATED="1604284975154" ID="ID_1988375141" MODIFIED="1604284981322" TEXT="Excluir item">
+<node CREATED="1604284983442" ID="ID_356079795" MODIFIED="1604284996604" TEXT="Confirmar se usu&#xe1;rio deseja excluir item"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
